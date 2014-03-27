a28ff6b8001ab5630cc88cf086876d8cd211d669
Janek Szynal <jan.szynal@gmail.com>
2014-03-27 11:08:32 +0100

    stworzenie projektu w Netbeans

---
+++ SO1_AK_JS/src/so1_ak_js/SO1_AK_JS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the temlate in the editor.
+ */
+
+package so1_ak_js;
+
+/**
+ *
+ * @author Janek
+ */
+public class SO1_AK_JS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
